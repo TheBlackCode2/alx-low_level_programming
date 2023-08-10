a928dada3a8acf826324af540ec1ba513341a40c
TheBlackCode2 <yassinew20w@gmail.com>
2023-08-10 01:22:59 -0700

    Update 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print text whitin puts function
+ *
+ * Return: 0 (EXIT_SUCCESS)
+ *
+*/
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
