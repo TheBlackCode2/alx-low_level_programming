eaaa59c505ef116f9c1bf67e6b602c8413d6d3e6
TheBlackCode2 <yassinew20w@gmail.com>
2023-08-15 03:54:35 -0700

    new Project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" forllowed by new line
+ *
+ * Return: 0 (EXIT_SUCCESS)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
