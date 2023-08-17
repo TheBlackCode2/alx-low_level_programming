b736a7ea48d119e004c829d862040e5afac8d981
TheBlackCode2 <yassinew20w@gmail.com>
2023-08-17 11:35:57 -0700

    New update

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always
+*/
+
+int main(void)
+{
+	long n = 612852475143;
+	long i, largest_factor;
+	double square = sqrt(n);
+
+	for (i = 0; i < square; i++)
+	{
+		if (n % i == 0)
+			largest_factor = n / i;
+	}
+
+	printf("%ld\n", largest_factor);
+
+	return (0);
+}
