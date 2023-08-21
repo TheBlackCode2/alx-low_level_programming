b56ef7dd42cf9797f913499dbce9c11d3a57ee13
TheBlackCode2 <yassinew20w@gmail.com>
2023-08-21 01:35:49 -0700

    New update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string for check
+*/
+
+void print_rev(char *s)
+{
+	int i = strlen(s) - 1;
+
+	for (; i >= 0; i--)
+		_putchar(*(s+i));
+
+	_putchar('\n');
+}
