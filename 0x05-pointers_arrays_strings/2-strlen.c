e6499ff32c893603e59c7c8a0fd7db51fd2ecf48
TheBlackCode2 <yassinew20w@gmail.com>
2023-08-21 01:23:58 -0700

    New update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strlen - get the length of a string
@@ -8,13 +9,5 @@
 
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
+	return (strlen(s));
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @str: string to print
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+}
