9e9214545b32326e42c81696b71209f505967d13
TheBlackCode2 <yassinew20w@gmail.com>
2023-09-01 21:08:56 +0100

    New update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,49 +10,49 @@
 int main(int argc, char *argv[])
 {
 
-    /* Declaration of variables */
-    int n[2]  = {0, 0};
-    int result;
-    int i;
+	/* Declaration of variables */
+	int n[2]  = {0, 0};
+	int result;
+	int i;
 
-    /* Chek for input arguments size */
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	/* Chek for input arguments size */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    /* Check for number digits and convert string to int */
-    for (i = 1; i < argc; i++)
-    {
-        int sign = 1;
-        int j;
+	/* Check for number digits and convert string to int */
+	for (i = 1; i < argc; i++)
+	{
+		int sign = 1;
+		int j;
 
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] >= '0' && argv[i][j] <= '9')
-            {
-                n[i - 1] *= 10;
-                n[i - 1] += argv[i][j] - '0';
-            }
-            else if (argv[i][j] == '-')
-            {
-                sign *= -1;
-            }
-            else
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+			{
+				n[i - 1] *= 10;
+				n[i - 1] += argv[i][j] - '0';
+			}
+			else if (argv[i][j] == '-')
+			{
+				sign *= -1;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 
-        n[i - 1] *= sign;
-    }
+		n[i - 1] *= sign;
+	}
     
 
-    /* Calc multiplication and display the result */
-    result = n[0] * n[1];
-    printf("%d\n", result);
+	/* Calc multiplication and display the result */
+	result = n[0] * n[1];
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,41 +10,39 @@
 int main(int argc, char *argv[])
 {
 
-    /* Declaration of variables */
-    int sum = 0;
-    int i;
+	/* Declaration of variables */
+	int sum = 0;
+	int i;
 
-    /* Check for number digits and calc sum */
-    for (i = 1; i < argc; i++)
-    {
-        int num = 0;
-        int sign = 1;
-        int j;
+	/* Check for number digits and calc sum */
+	for (i = 1; i < argc; i++)
+	{
+		int num = 0;
+		int sign = 1;
+		int j;
 
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] >= '0' && argv[i][j] <= '9')
-            {
-                num *= 10;
-                num += argv[i][j] - '0';
-            }
-            else if (argv[i][j] == '-')
-            {
-                sign *= -1;
-            }
-            else
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+			{
+				num *= 10;
+				num += argv[i][j] - '0';
+			}
+			else if (argv[i][j] == '-')
+			{
+				sign *= -1;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 
-        sum += (num * sign);
-    }
-    
+		sum += (num * sign);
+	}
 
-    /* Calc multiplication and display the result */
-    printf("%d\n", sum);
-
-    return (0);
+	/* Calc multiplication and display the result */
+	printf("%d\n", sum);
+	return (0);
 }
