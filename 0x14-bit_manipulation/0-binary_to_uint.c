9d74190fecd113f094baaedbbea68c2593f39664
TheBlackCode2 <yassinew20w@gmail.com>
2023-09-28 11:39:28 +0100

    New update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,58 +1,25 @@
 #include "main.h"
 
 /**
- * power_of - get the power of a number base to other number
- * @n1: given input
- * @n2: power
- * Return: power of n1 base to n2
-*/
-
-int power_of(int n1, int n2)
-{
-	int pow = n1;
-
-	if (n2 == 0)
-		return (1);
-
-	while((n2 - 1) > 0)
-	{
-		n2--;
-		pow *= n1;
-	}
-
-	return (pow);
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * Return: the converted number, or 0
-*/
-
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0;
 	int i;
-	unsigned int num = 0;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while(*(b + len) != '\0')
+	for (i = 0; b[i]; i++)
 	{
-		if (*(b + len) == '0' || *(b + len) == '1')
-			len++;
-		else
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	for (i = len; i > 0; i--)
-	{
-		if (*(b + (len - i)) == '0')
-			continue;
-
-		num += power_of(2, i - 1);
-	}
-
-	return (num);
+	return (dec_val);
 }
