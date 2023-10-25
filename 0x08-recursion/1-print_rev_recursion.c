22866ae13c3888a57198c293876abcb8fca8cc6e
TheBlackCode2 <yassinew20w@gmail.com>
2023-10-25 11:53:45 +0100

    New update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,7 +7,10 @@
 */
 void _print_rev_recursion(char *s)
 {
-	if (*(s + 1) != '\0')
-		_print_rev_recursion(s + 1);
-	_putchar(*s);
+	if (*s != '\0')
+	{
+		if (*(s + 1) != '\0')
+			_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
