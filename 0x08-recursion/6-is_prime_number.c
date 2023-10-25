a895b8942f707c1ac9e75aa2e5e2e3f95861dad7
TheBlackCode2 <yassinew20w@gmail.com>
2023-10-25 15:56:07 +0100

    New update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * search_for_prime - checks if there is a prime for the given number
+ * @i: searching number
+ * @n: the input number
+ * Return: result of search
+*/
+int search_for_prime(int i, int n)
+{
+	if (i == n)
+		return (1);
+	if (n % i == 0 || i > n)
+		return (0);
+	return (search_for_prime(i + 1, n));
+}
+
+/**
+ * is_prime_number - checks if the given input is a prime number
+ * @n: the input number
+ * Return: 1 if the input integer is a prime number, otherwise 0
+*/
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (search_for_prime(2, n));
+}
