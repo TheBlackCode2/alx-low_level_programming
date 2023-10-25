4a9623ec5dd5fc6cee14e577ea7652a32c04c679
TheBlackCode2 <yassinew20w@gmail.com>
2023-10-25 17:34:26 +0100

    New update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,9 +7,9 @@
 */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        return (1 + _strlen_recursion(++s));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(++s));
 }
 
 /**
@@ -25,7 +25,7 @@ int process(char *s, int l, int i)
 	if (i >= l)
 		return (1);
 	if (*(s + i) == *(s + l - 1))
-		return (process(s,l - 1, i + 1));
+		return (process(s, l - 1, i + 1));
 	return (0);
 }
 
