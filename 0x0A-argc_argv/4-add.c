500a00c7ddd45d4a9076d54dde6863a95cba8925
TheBlackCode2 <yassinew20w@gmail.com>
2023-10-27 12:56:45 +0100

    New update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: the number of commands arguments
+ * @argv: an array containing the program command line arguments
+ * Return: always 0
+*/
+int main(int argc __attribute__((unused)), char **argv)
+{
+	int i;
+	int sum __attribute__((unused)) = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		int j;
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
