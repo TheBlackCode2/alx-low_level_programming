2a526b2b0033e03d8d26f8283f394e7424f20216
TheBlackCode2 <yassinew20w@gmail.com>
2023-10-27 07:57:17 +0100

    New update

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes
+ * Return: concatenate string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	while (*(src + j) != '\0' && j < n)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,7 @@
+# 0x09. C - Static libraries
+
+This repo is about figure what is a **"C" library:**
+
+> - How to create a "C" library.
+> - How to use it in a program.
+> - What is the difference bertween a dynamic and static library
