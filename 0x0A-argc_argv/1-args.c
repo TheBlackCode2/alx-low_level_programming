61e04fff5fe08a363bd12c85c01fd36b049c9697
TheBlackCode2 <yassinew20w@gmail.com>
2023-10-27 12:14:55 +0100

    New update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the number of commands arguments
+ * @argv: an array containing the program command line arguments
+ * Return: always 0
+*/
+int main(int argc , char **argv __attribute__((unused)))
+{
+	printf("%u\n", (argc - 1));
+	return (0);
+}
