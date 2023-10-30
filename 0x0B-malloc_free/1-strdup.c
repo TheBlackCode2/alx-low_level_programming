9944e16828c121f75aabad0de9cc98c2042438f2
TheBlackCode2 <yassinew20w@gmail.com>
2023-10-30 08:35:12 +0100

    New update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_length - gets the length of the given string
+ * @str: the input string
+ * Return: the length of the input string
+*/
+int get_length(char *str)
+{
+	if(*str == '\0')
+		return (0);
+	return (1 + get_length(++str));
+}
+
+/**
+ * _strdup - gets a pointer to a newly allocated
+ *           space in memory, which contains a copy
+ *           of the string given as a parameter.
+ * @str: the input string
+ * Return: a pointer to a new duplicate string,
+ *         or NULL if the given string is NULL
+*/
+char *_strdup(char *str)
+{
+	int size;
+	char *buffer;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = get_length(str);
+	buffer = (char *) malloc(sizeof(char) * size);
+
+	if (buffer)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			buffer[i] = str[i];
+	}
+
+	return (buffer);
+}
