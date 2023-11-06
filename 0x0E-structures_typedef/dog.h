b207c14ff283b0a574f67ed1a67fb7f4bce77876
TheBlackCode2 <yassinew20w@gmail.com>
2023-11-06 14:13:21 +0100

    New update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdlib.h>
-
-/*typedef struct dog dog_t;*/
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
