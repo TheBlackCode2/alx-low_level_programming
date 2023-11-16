4b1aaefc6b070d0a4ea6fcb7a56bd19642b488dc
TheBlackCode2 <yassinew20w@gmail.com>
2023-11-16 11:15:07 +0100

    New update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the first node
+ * Return: the number of nodes printed
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+	
+	for (i = 0; h; i++, h++)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+# 0x12. C - Singly linked lists
+
+this repo is about figure:
+
+> - When and why using linked lists vs arrays.
+> - How to build and use linked lists.
