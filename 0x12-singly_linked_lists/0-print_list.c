d5d28dbfdf56bf1a7abfb403ac3e29e2620bb4f2
TheBlackCode2 <yassinew20w@gmail.com>
2023-11-16 11:20:09 +0100

    New update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,17 +6,17 @@
  * @h: pointer to the first node
  * Return: the number of nodes printed
 */
-
 size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	for (i = 0; h; i++, h++)
+	for (i = 0; h; i++)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 	}
 	return (i);
 }
